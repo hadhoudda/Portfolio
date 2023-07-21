9846a1508ac16d6258c5571c4a68762d49472fdf
Houda Ben Abdallah <ahhouda@yahoo.fr>
2023-07-21 14:17:36 +0200

    component header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,6 +7,51 @@ function Header() {
         <header className="header">
             <nav className="nav container">
                 <a href="index.html" className="nav_logo">Smith</a>
+                <div className="nav_menu">
+                    <ul className="nav_list grid">
+                        <li className="nav_item">
+                            <a href="#home" className="nav_link">
+                                <i className="uil uil-estate nav_icon"></i>
+                                Home
+                            </a>
+                        </li>
+                        <li className="nav_item">
+                            <a href="#about" className="nav_link">
+                                <i className="uil uil-user nav_icon"></i>
+                                About
+                            </a>
+                        </li>
+                        <li className="nav_item">
+                            <a href="#skills" className="nav_link">
+                                <i className="uil uil-file-alt nav_icon"></i>
+                                Skills
+                            </a>
+                        </li>
+                        <li className="nav_item">
+                            <a href="#services" className="nav_link">
+                                <i className="uil uil-briefcase-alt nav_icon"></i>
+                                Services
+                            </a>
+                        </li>
+                        <li className="nav_item">
+                            <a href="#portfolio" className="nav_link">
+                                <i className="uil uil-scenery nav_icon"></i>
+                                Portfolio
+                            </a>
+                        </li>
+                        <li className="nav_item">
+                            <a href="#contact" className="nav_link">
+                                <i className="uil uil-message nav_icon"></i>
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
+
+                    <i className="uil uil-times nav_close"></i>
+                </div>
+                <div className="nav_toggle">
+                    <i className="uil uil-apps "></i>
+                </div>
             </nav>
         </header>
     )
