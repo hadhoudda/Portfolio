301e98df06249dd55f96230115e7591344ea2511
Houda Ben Abdallah <ahhouda@yahoo.fr>
2023-07-24 14:48:10 +0200

    component profil

--- frontend/src/components/Profil.jsx
+++ frontend/src/components/Profil.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-import Navbar from "./Navbar";
 import '../styles/profil.css'
+import profil from "../assets/profil.jpg"
 
 function Profil() {
     return (
         <div className="profil" id="profil">
             
             {/* <Navbar className="nav-fixed"/> */}
-            <p>houda</p>
+            <h2>A PROPOS DE MOI</h2>
+            <div className="contenu-profil">
+                <img className ="photo-profil" src={profil} alt='profil__houda' />
+                <p >
+                    Hello ! Je m’appelle Houda, développeur Web junior
+                </p>
+
+            </div>
+            
         </div>
     )
 }
