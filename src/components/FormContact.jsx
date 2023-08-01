151ba72829288d9d206804991e3b2023760200c6
Houda Ben Abdallah <ahhouda@yahoo.fr>
2023-08-01 19:43:10 +0200

    contact

--- src/components/FormContact.jsx
+++ src/components/FormContact.jsx
@@ -1,32 +1,47 @@
-import {useRef} from 'react';
+import React, { useRef} from 'react';
+import emailjs from '@emailjs/browser';
 import '../styles/formcontact.css'
 
 
 //const FormContact = ()=> {
 function FormContact() {
     const form = useRef()
-return (
-    <div className="container-form">
-        <h2>Contact</h2>
-        <form ref={form} onSubmit={} className='form-contact'>
-            <input type="text" 
-                placeholder='votre nom'
-                name='user-name'
-                required />
-            <input type="email" 
-                placeholder='Email'
-                name='user-email'
-                required />
-            <input type="text" 
-                placeholder='Sujet'
-                name='subject'
-                required />
-            <textarea name="message" cols="30" rows="10"></textarea>
-            <button type='submit' className='btn-message'>envoyer message</button>
-        </form>
-    </div>
-  
-  )
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        // emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+        emailjs.sendForm('service_xbw7xvx', 'template_x1ohdje', form.current, 'X1Cf5ovioyGwNMk3Y')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset();
+      };
+
+    return (
+        <div className="container-form">
+            <h2>Contact</h2>
+            <form ref={form} onSubmit={sendEmail} className='form-contact'>
+                <input type="text" 
+                    placeholder='votre nom'
+                    name='user_name'
+                    required />
+                <input type="email" 
+                    placeholder='Email'
+                    name='user_email'
+                    required />
+                <input type="text" 
+                    placeholder='Sujet'
+                    name='subject'
+                    required />
+                <textarea name="message" cols="30" rows="10"></textarea>
+                <button type='submit' className='btn-message'>envoyer message</button>
+            </form>
+        </div>
+    
+    )
   
   }
 
