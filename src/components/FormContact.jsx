8b8c4127dde5f798941694c2c8a5efa6d5ad8216
Houda Ben Abdallah <ahhouda@yahoo.fr>
2023-08-02 02:05:56 +0200

    css form contact

--- src/components/FormContact.jsx
+++ src/components/FormContact.jsx
@@ -1,16 +1,16 @@
 import React, { useRef} from 'react';
 import emailjs from '@emailjs/browser';
+//require('dotenv').config();
 import '../styles/formcontact.css'
 
-
-//const FormContact = ()=> {
 function FormContact() {
     const form = useRef()
 
     const sendEmail = (e) => {
         e.preventDefault();
     
-        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+        //emailjs.sendForm(process.env.YOUR_SERVICE_ID, process.env.YOUR_TEMPLATE_ID , form.current, process.env.YOUR_PUBLIC_KEY)
+        emailjs.sendForm('service_xbw7xvx', 'template_x1ohdje', form.current, 'X1Cf5ovioyGwNMk3Y')
           .then((result) => {
               console.log(result.text);
           }, (error) => {
@@ -21,23 +21,38 @@ function FormContact() {
 
     return (
         <div className="container-form">
-            <h2>Contact</h2>
-            <form ref={form} onSubmit={sendEmail} className='form-contact'>
-                <input type="text" 
-                    placeholder='votre nom'
-                    name='user_name'
-                    required />
-                <input type="email" 
-                    placeholder='Email'
-                    name='user_email'
-                    required />
-                <input type="text" 
-                    placeholder='Sujet'
-                    name='subject'
-                    required />
-                <textarea name="message" cols="30" rows="10"></textarea>
-                <button type='submit' className='btn-message'>envoyer message</button>
-            </form>
+            <h2 className='title-contact'>CONTACTEZ-MOI</h2>
+            <p className="text-contact">N'hésitez pas à me contacter,
+                 je vous répondrai dans les plus brefs délais.
+            </p>
+            <div className="content-form-contact">
+                <form ref={form} onSubmit={sendEmail} className='form-contact'>
+                    <input type="text" 
+                        placeholder='votre nom*'
+                        name='user_name'
+                        required />
+                    <input type="email" 
+                        placeholder='Votre email*'
+                        name='user_email'
+                        required />
+                    <input type="text" 
+                        placeholder='Sujet'
+                        name='subject'
+                        />
+                    <textarea name="message" placeholder='Votre message*' ></textarea>
+                    <button type='submit' className='btn-message'>Envoyer </button>
+                    <p className="text-form">* Champ obligatoire</p>
+                </form>
+                <div className="info-contact">
+                    <p className="text-info-contact">Contact direct :</p>
+                    <div className="icone-form">
+                        <a href="https://wa.me/+33622464454"><i className='bx bxl-whatsapp'></i></a>
+                        <a href="mailto:ahhouda@yahoo.fr"><i className='bx bx-mail-send'></i></a>
+                        <a href="tel:0587878787"><i class='bx bxs-phone'></i></a>
+                    </div>
+                    
+                </div>
+            </div>
         </div>
     
     )
