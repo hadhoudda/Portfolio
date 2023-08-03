9594e88975f25c8fde2cdc0d935353505eca6387
Houda Ben Abdallah <ahhouda@yahoo.fr>
2023-08-03 13:20:05 +0200

    +tags

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,15 +7,13 @@ export default function Project({project}) {
     return( 
         <>
         <div className='card-project' >
-            <div className="project ">
                 <img className='img-project' src={ project.picture }  alt={"l'image de " + project.title}/>
                 <h4 className='title-card'> {project.title}</h4>
                 <Link to={project.github} target="_blank"><button className='btn-project'>Voir le code <i className='bx bx-chevron-right'></i></button> </Link>
                 <Link to={project.site} target="_blank"><button className='btn-project'>Voir le site <i className='bx bx-chevron-right'></i></button> </Link>
-            </div>
         </div>
-        <div className="project description-project">
-            <h4> {project.title}</h4>
+        <div className="description-project">
+            <h4 className='title-description'> {project.title}</h4>
             <p className="text-project"> {project.description} </p>
 
         </div>
