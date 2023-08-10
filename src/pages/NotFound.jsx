fc78ac02c1c413bae9c1f04737a0a2fa0963de60
Houda Ben Abdallah <ahhouda@yahoo.fr>
2023-08-10 11:37:00 +0200

    update

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-//import '../styles/notfound.css';
+import '../styles/notfound.css';
 import Header from "../components/Header";
 import Footer from '../components/Footer';
 
